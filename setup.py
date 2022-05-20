dadced0263f0181f01a2823378b987de194ff582
Will <warliss98@gmail.com>
2022-05-20 08:25:52 -0400

    WIP

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
         install_requires=requirements,
         version='1.0.0',
         python='>=3.10.4',
-        description="A general framework for minimizing custom loss functions using Scipy's and Scikit-Learn."
+        description="A general framework for minimizing custom loss functions using Scipy's and Scikit-Learn.",
         classifiers=['Programming Language :: Python :: 3'],
     )
