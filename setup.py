53fc6fd571845a96ee396c00f7367f5556fba8b5
Will <warliss98@gmail.com>
2022-05-29 15:59:31 -0400

    Added option for forcing pip update

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 import os
+import sys
 from setuptools import setup, find_packages
 
 REQS = './requirements/requirements.txt'
 
-with open(REQS, 'r') as infile:
-    requirements = infile.read().split('\n')
-
-packages = list(find_packages())
-
 if __name__ == '__main__':
 
-    #os.system('python3 -m pip install -U pip setuptools')
-    os.system(f'pip3 install -r {REQS}')
+    with open(REQS, 'r') as infile:
+        requirements = infile.read().split('\n')
+
+    packages = list(find_packages())
+
+    if '-P' in sys.argv:
+        os.system('python3 -m pip install -U pip setuptools')
+        os.system(f'pip3 install -r {REQS}')
+        sys.argv.remove('-P')
 
     setup(
         name='General Loss Minimizers',
         packages=packages,
         install_requires=requirements,
         version='1.0.0',
-        python='>=3.10.4',
-        description="A general framework for minimizing custom loss functions using Scipy's and Scikit-Learn.",
+        python='>=3.10.0',
+        description="A general framework for minimizing custom loss functions using Scipy and Scikit-Learn.",
         classifiers=['Programming Language :: Python :: 3'],
     )
